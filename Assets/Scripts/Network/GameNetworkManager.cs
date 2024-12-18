cc5b2ece34a05a419f5324d1863355033006f3c4
franciszek-majkusiak <fmajkusiak@gmail.com>
2024-12-18 01:34:44 +0100

    Added GUI for lobby hosting/joining nad lobby room

--- Assets/Scripts/Network/GameNetworkManager.cs
+++ Assets/Scripts/Network/GameNetworkManager.cs
@@ -1,18 +1,13 @@
 using Mirror;
+using Mirror.Discovery;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class GameNetworkManager : NetworkManager
+public class GameNetworkManager : NetworkRoomManager
 {
     public GameObject mapPrefab;
-
     public GameMapManager gameMapManager;
-    public override void OnStartServer()
-    {
-        SpawnMap();
-    }
-
-
     private void SpawnMap()
     {
         GameObject mapInstance = Instantiate(mapPrefab, Vector3.zero, Quaternion.identity);
@@ -20,16 +15,47 @@ public class GameNetworkManager : NetworkManager
         gameMapManager = mapInstance.GetComponent<GameMapManager>();
     }
 
-    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    public override void OnRoomServerSceneChanged(string sceneName)
     {
-        Debug.Log($"Trying to instantiate player {conn.connectionId}");
-        Transform startPos = GetStartPosition();
-        GameObject player = startPos != null
-            ? Instantiate(playerPrefab, startPos.position, startPos.rotation)
-            : Instantiate(playerPrefab);
+        base.OnRoomServerSceneChanged(sceneName);
+        if(sceneName == GameplayScene)
+            SpawnMap();
+    }
 
-        player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
-        Debug.Log($"Instatiating player {player.name}");
-        NetworkServer.AddPlayerForConnection(conn, player);
+    public override void OnGUI()
+    {
+        if (Utils.IsSceneActive(GameplayScene))
+        {
+            GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
+            if (NetworkServer.active)
+            {
+                if (GUILayout.Button("Return to Room"))
+                    ServerChangeScene(RoomScene);
+            }
+            else
+            {
+                if (GUILayout.Button("Leave"))
+                    StopClient();
+            }
+            GUILayout.EndArea();
+        }
+
+        if (Utils.IsSceneActive(RoomScene))
+        {
+            GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
+            if (GUILayout.Button("Return to main menu"))
+            {
+                if (NetworkServer.active && NetworkClient.isConnected)
+                {
+                    StopHost();
+                }
+                else if (NetworkClient.isConnected)
+                {
+                    StopClient();
+                }
+            }
+            GUILayout.EndArea();
+            GUI.Box(new Rect(0, Screen.height * 0.1f, Screen.width, Screen.height * 0.5f), "PLAYERS");
+        }
     }
 }
---
+++ Assets/Scripts/NetworkDiscoveryGUI.cs
@@ -0,0 +1,122 @@
+using Mirror;
+using Mirror.Discovery;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkDiscoveryGUI : MonoBehaviour
+{
+    public NetworkDiscovery networkDiscovery;
+    readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
+
+
+    private bool showServers = false;
+    private float buttonHeight = 50;
+
+    private GUIStyle titleStyle;
+    private void Awake()
+    {
+        networkDiscovery = GetComponent<NetworkDiscovery>();
+        UnityEditor.Events.UnityEventTools.AddPersistentListener(networkDiscovery.OnServerFound, OnDiscoveredServer);
+        UnityEditor.Undo.RecordObjects(new Object[] { this, networkDiscovery }, "Set NetworkDiscovery");
+    }
+
+    public void OnGUI()
+    {
+        if (NetworkManager.singleton == null)
+            return;
+
+        if (!NetworkClient.isConnected && !NetworkServer.active && !NetworkClient.active)
+        {
+            drawTitle();
+            if (showServers)
+                drawServerListGUI();
+            else
+                drawButtonsGUI();
+        }
+    }
+
+    private void drawTitle()
+    {
+        Rect textRect = new Rect(0, 0, Screen.width, Screen.height * 0.5f);
+        GUI.Label(textRect, "Czarodziejki ze Skaryszewa", new GUIStyle(GUI.skin.label)
+        {
+            fontSize = 80,
+            alignment = TextAnchor.MiddleCenter,
+            wordWrap = true
+        });
+    }
+
+    private void drawButtonsGUI()
+    {
+        var buttonStyle = new GUIStyle(GUI.skin.button)
+        {
+            fontSize = 48,
+            alignment = TextAnchor.MiddleCenter
+        };
+        GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.5f));
+        GUILayout.BeginVertical();
+
+        if (GUILayout.Button("Start Host", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
+        {
+            discoveredServers.Clear();
+            NetworkManager.singleton.StartHost();
+            networkDiscovery.AdvertiseServer();
+        }
+        GUILayout.Space(50);
+        if (GUILayout.Button("Find Servers", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
+        {
+            discoveredServers.Clear();
+            networkDiscovery.StartDiscovery();
+            showServers = true;
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
+    private void drawServerListGUI()
+    {
+        var buttonStyle = new GUIStyle(GUI.skin.button)
+        {
+            fontSize = 48,
+            alignment = TextAnchor.MiddleCenter
+        };
+        GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.5f));
+        GUILayout.BeginScrollView(new Vector2(0, 0));
+        foreach (ServerResponse info in discoveredServers.Values)
+            if (GUILayout.Button(info.EndPoint.Address.ToString(), buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
+                Connect(info);
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
+        if (GUILayout.Button("Back"))
+        {
+            showServers = false;
+        }
+        GUILayout.EndArea();
+    }
+    public void HostGame()
+    {
+        discoveredServers.Clear();
+        NetworkManager.singleton.StartHost();
+        networkDiscovery.AdvertiseServer();
+    }
+
+    public void FindServers()
+    {
+        discoveredServers.Clear();
+        networkDiscovery.StartDiscovery();
+    }
+
+    void Connect(ServerResponse info)
+    {
+        networkDiscovery.StopDiscovery();
+        NetworkManager.singleton.StartClient(info.uri);
+    }
+
+    public void OnDiscoveredServer(ServerResponse info)
+    {
+        // Note that you can check the versioning to decide if you can connect to the server or not using this method
+        discoveredServers[info.serverId] = info;
+    }
+}
