7fd1f254a486687a8d111114a54b4253476aefe7
franciszek-majkusiak <fmajkusiak@gmail.com>
2025-01-06 21:45:40 +0100

    Added player color selection

--- Assets/Scripts/Network/RoomPlayer.cs
+++ Assets/Scripts/Network/RoomPlayer.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class RoomPlayer : NetworkRoomPlayer
 {
+    [SyncVar]
+    public int ColorID;
+
     public override void OnGUI()
     {
         if (!showRoomGUI)
@@ -17,17 +20,22 @@ public class RoomPlayer : NetworkRoomPlayer
             if (!Utils.IsSceneActive(room.RoomScene))
                 return;
 
-            DrawPlayerReadyState();
-            DrawPlayerReadyButton();
+            DrawPlayerState();
+            if (NetworkClient.active && isLocalPlayer)
+            {
+                DrawPlayerSelection();
+                DrawPlayerReadyButton();
+            }
         }
     }
 
-    void DrawPlayerReadyState()
+    void DrawPlayerState()
     {
-        float areaHeight = Screen.height * 0.4f;
+        float areaHeight = Screen.height * 0.2f;
         float areaWidth = Screen.width * 0.2f;
         float spacing = Screen.width * 0.05f;
-        GUILayout.BeginArea(new Rect((index + 1) * spacing + (index * areaWidth), Screen.height * 0.2f, areaWidth, areaHeight));
+        
+        GUILayout.BeginArea(new Rect((index + 1) * spacing + (index * areaWidth), Screen.height * 0.1f, areaWidth, areaHeight));
         GUILayout.Label($"Player [{index + 1}]", new GUIStyle(GUI.skin.label)
         {
             fontSize = 36,
@@ -47,7 +55,7 @@ public class RoomPlayer : NetworkRoomPlayer
 
         if (((isServer && index > 0) || isServerOnly) && GUILayout.Button("REMOVE", GUILayout.ExpandWidth(true)))
         {
-            GUILayout.Space(100);
+            //GUILayout.Space(100);
             GetComponent<NetworkIdentity>().connectionToClient.Disconnect();
         }
         GUILayout.EndArea();
@@ -57,7 +65,7 @@ public class RoomPlayer : NetworkRoomPlayer
     {
         if (NetworkClient.active && isLocalPlayer)
         {
-            GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.65f, Screen.width * 0.5f, Screen.height * 0.3f));
+            GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.8f, Screen.width * 0.5f, Screen.height * 0.2f));
             var buttonStyle = new GUIStyle(GUI.skin.button)
             {
                 fontSize = 48,
@@ -76,5 +84,41 @@ public class RoomPlayer : NetworkRoomPlayer
 
             GUILayout.EndArea();
         }
+    }
+
+    void DrawPlayerSelection()
+    {
+        GameNetworkManager gameManager = NetworkManager.singleton as GameNetworkManager;
+        
+        float size = Screen.height * 0.4f;
+
+        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f - size / 2.0f, size, size));
+        if (gameManager.playerTextures[ColorID] != null)
+        {
+            // Stretch the texture to fill the whole box (200x200)
+            GUI.DrawTexture(new Rect(0, 0, size, size), gameManager.playerTextures[ColorID], ScaleMode.StretchToFill);
+        }
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f + size / 2.0f, size, Screen.height * 0.05f));
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("<", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(50)))
+        {
+            int colorID = (ColorID - 1 + gameManager.playerTextures.Length) % gameManager.playerTextures.Length;
+            CmdChangeColorID(colorID);
+        }
+        if (GUILayout.Button(">", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(50)))
+        {
+            int colorID = (ColorID + 1) % gameManager.playerTextures.Length;
+            CmdChangeColorID(colorID);
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+    }
+
+    [Command]
+    public void CmdChangeColorID(int newColorID)
+    {
+        ColorID = newColorID;
     }
 }
