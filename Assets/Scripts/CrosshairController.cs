09b9d8acf3fa0699b5e05b76026c8a94adec5ba2
feedop <filipskrzeczkowski@gmail.com>
2025-01-07 23:52:47 +0100

    Add crosshair and projectile lighting

--- Assets/Scripts/CrosshairController.cs
+++ Assets/Scripts/CrosshairController.cs
@@ -1,14 +1,14 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.InputSystem;
-using static UnityEngine.Rendering.DebugUI.Table;
+using UnityEngine.Rendering.Universal;
 
 public class CrosshairController : MonoBehaviour
 {
     public float range;
     public float idleSpinVelocity;
     public float firingSpinVelocity;
+	public GameObject pointLight;
 
 	private Quaternion spin;
 	private InputAction fireAction;
@@ -17,9 +17,11 @@ public class CrosshairController : MonoBehaviour
 	void Start()
     {
 		spin = Quaternion.identity;
-		transform.GetComponent<SpriteRenderer>().color = transform.parent.GetComponent<PlayerController>().primaryColor;
+		var color = transform.parent.GetComponent<PlayerController>().primaryColor;
+        GetComponent<SpriteRenderer>().color = color;
+        pointLight.GetComponent<Light2D>().color = color;
 
-		fireAction = InputSystem.actions.FindAction("Attack");
+        fireAction = InputSystem.actions.FindAction("Attack");
     }
 
     // Update is called once per frame
@@ -28,6 +30,7 @@ public class CrosshairController : MonoBehaviour
         // Update position
         Vector3 mousePos = Mouse.current.position.ReadValue();
         Vector3 parentPos = Camera.main.WorldToScreenPoint(transform.parent.gameObject.transform.position);
+        parentPos.z = 0.0f;
 		Vector3 aimDirection = Vector3.Normalize(mousePos - parentPos);
 		Vector3 scaledAimDirection = aimDirection * range;
 
@@ -46,5 +49,7 @@ public class CrosshairController : MonoBehaviour
 		var matchQuaternion = Quaternion.AngleAxis(angle, new Vector3(0,0,1));
 		totalRotation = matchQuaternion * totalRotation;
 		transform.localRotation = totalRotation;
+
+        pointLight.transform.rotation = Quaternion.identity;
     }
 }
