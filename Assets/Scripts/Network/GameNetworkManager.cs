4001aa865de593867a133e1bd6baf48e675b74ea
Szymon <114956705+simonsrz@users.noreply.github.com>
2025-01-25 16:07:06 +0100

    UI improvement (#119)
    * UI refactor
    
    * post-cr fix
    
    * fixed button positions
    
    * spectator mode adjustments

--- Assets/Scripts/Network/GameNetworkManager.cs
+++ Assets/Scripts/Network/GameNetworkManager.cs
@@ -38,17 +38,27 @@ public class GameNetworkManager : NetworkRoomManager
 
     public override void OnGUI()
     {
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.font = Resources.Load<Font>("Fonts/VT323-Regular");
+        buttonStyle.normal.textColor = Color.black;
+        buttonStyle.hover.textColor = Color.black;
+        buttonStyle.active.textColor = Color.black;
+        buttonStyle.normal.background = MakeTex(2, 2, new Color(0.9607844f, 0.7294118f, 0.9215687f));
+        buttonStyle.hover.background = MakeTex(2, 2, new Color(1f, 0.8820755f, 0.980345f));
+        buttonStyle.active.background = MakeTex(2, 2, new Color(0.9811321f, 0.513706f, 0.9019072f));
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+
         if (Utils.IsSceneActive(GameplayScene))
         {
             GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
             if (NetworkServer.active)
             {
-                if (GUILayout.Button("Return to Room"))
+                if (GUILayout.Button("Return to Room", buttonStyle))
                     ServerChangeScene(RoomScene);
             }
             else
             {
-                if (GUILayout.Button("Leave"))
+                if (GUILayout.Button("Leave", buttonStyle))
                     StopClient();
             }
             GUILayout.EndArea();
@@ -56,19 +66,37 @@ public class GameNetworkManager : NetworkRoomManager
 
         if (Utils.IsSceneActive(RoomScene))
         {
-            GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
-            if (GUILayout.Button("Return to main menu"))
+            GUILayout.BeginArea(new Rect(Screen.width - 400f, 10f, 390f, 60f));
+            buttonStyle.fontSize = 30;
+            if (GUILayout.Button("Return to main menu", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
             {
-                if (NetworkServer.active && NetworkClient.isConnected)
-                {
-                    StopHost();
-                }
-                else if (NetworkClient.isConnected)
-                {
-                    StopClient();
-                }
+                ReturnToMainMenu();
             }
             GUILayout.EndArea();
         }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            StopHost();
+        }
+        else if (NetworkClient.isConnected)
+        {
+            StopClient();
+        }
+    }
+    private Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; i++)
+        {
+            pix[i] = col;
+        }
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
     }
 }
