35c6fe7e6f6b1668693f583cd3d21bcc2b2cfdaa
simonsrz <szymonslusarz01@gmail.com>
2025-01-25 13:40:28 +0100

    UI refactor

--- Assets/Scripts/Network/RoomPlayer.cs
+++ Assets/Scripts/Network/RoomPlayer.cs
@@ -1,5 +1,7 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomPlayer : NetworkRoomPlayer
 {
@@ -31,54 +33,63 @@ public class RoomPlayer : NetworkRoomPlayer
 
     void DrawPlayerState()
     {
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+        labelStyle.fontSize = 70;
+        labelStyle.font = Resources.Load<Font>("Fonts/VT323-Regular");
+        labelStyle.normal.textColor = Color.black;
+        labelStyle.alignment = TextAnchor.MiddleCenter;
+
         float areaHeight = Screen.height * 0.2f;
         float areaWidth = Screen.width * 0.2f;
         float spacing = Screen.width * 0.05f;
-        
-        GUILayout.BeginArea(new Rect((index + 1) * spacing + (index * areaWidth), Screen.height * 0.1f, areaWidth, areaHeight));
-        GUILayout.Label($"Player [{index + 1}]", new GUIStyle(GUI.skin.label)
-        {
-            fontSize = 36,
-            alignment = TextAnchor.MiddleCenter,
-            wordWrap = true,
-        }, GUILayout.ExpandWidth(true));
-        GUIStyle readyStyle = new GUIStyle(GUI.skin.label)
-        {
-            fontSize = 24,
-            alignment = TextAnchor.MiddleCenter,
-            wordWrap = true
-        };
-        if (readyToBegin)
-            GUILayout.Label("Ready", readyStyle, GUILayout.ExpandWidth(true));
-        else
-            GUILayout.Label("Not Ready", readyStyle, GUILayout.ExpandWidth(true));
 
-        if (((isServer && index > 0) || isServerOnly) && GUILayout.Button("REMOVE", GUILayout.ExpandWidth(true)))
+        GUIStyle area = new GUIStyle();
+        if(readyToBegin)
+            area.normal.background = MakeTex(2, 2, new Color(0.439f, 1.0f, 0.518f));
+        else
+            area.normal.background = MakeTex(2, 2, new Color(0.831f, 0.071f, 0.071f));
+
+        GUILayout.BeginArea(new Rect((index + 1) * spacing + (index * areaWidth), Screen.height * 0.1f, areaWidth, areaHeight));
+        GUILayout.BeginVertical(area);
+        GUILayout.Label($"Player {index + 1}", labelStyle, GUILayout.ExpandWidth(true));
+
+        if (((isServer && index > 0) || isServerOnly) && GUILayout.Button("REMOVE", new GUIStyle(GUI.skin.button)
         {
-            //GUILayout.Space(100);
+            fontSize = 50,
+            font = Resources.Load<Font>("Fonts/VT323-Regular"),
+        }, GUILayout.ExpandWidth(true), GUILayout.Height(areaHeight * 0.2f)))
+        {
+            GUILayout.Space(50);
             GetComponent<NetworkIdentity>().connectionToClient.Disconnect();
         }
+        GUILayout.EndVertical();
         GUILayout.EndArea();
     }
 
     void DrawPlayerReadyButton()
     {
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.fontSize = 100;
+        buttonStyle.font = Resources.Load<Font>("Fonts/VT323-Regular");
+        buttonStyle.normal.textColor = Color.black;
+        buttonStyle.hover.textColor = Color.black;
+        buttonStyle.active.textColor = Color.black;
+        buttonStyle.normal.background = MakeTex(2, 2, new Color(0.9607844f, 0.7294118f, 0.9215687f));
+        buttonStyle.hover.background = MakeTex(2, 2, new Color(1f, 0.8820755f, 0.980345f));
+        buttonStyle.active.background = MakeTex(2, 2, new Color(0.9811321f, 0.513706f, 0.9019072f));
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+
         if (NetworkClient.active && isLocalPlayer)
         {
             GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.8f, Screen.width * 0.5f, Screen.height * 0.2f));
-            var buttonStyle = new GUIStyle(GUI.skin.button)
-            {
-                fontSize = 48,
-                alignment = TextAnchor.MiddleCenter
-            };
             if (readyToBegin)
             {
-                if (GUILayout.Button("Cancel", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
+                if (GUILayout.Button("CANCEL", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
                     CmdChangeReadyState(false);
             }
             else
             {
-                if (GUILayout.Button("Ready", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
+                if (GUILayout.Button("READY", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
                     CmdChangeReadyState(true);
             }
 
@@ -91,8 +102,10 @@ public class RoomPlayer : NetworkRoomPlayer
         GameNetworkManager gameManager = NetworkManager.singleton as GameNetworkManager;
         
         float size = Screen.height * 0.4f;
+        float arrowsHeight = Screen.height * 0.05f;
+        float spacing = 20.0f;
 
-        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f - size / 2.0f, size, size));
+        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f - size / 2.0f, size, size + spacing + arrowsHeight));
         if (gameManager.playerTextures[ColorID] != null)
         {
             // Stretch the texture to fill the whole box (200x200)
@@ -100,20 +113,58 @@ public class RoomPlayer : NetworkRoomPlayer
         }
         GUILayout.EndArea();
 
-        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f + size / 2.0f, size, Screen.height * 0.05f));
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.fontSize = 36;
+        buttonStyle.font = Resources.Load<Font>("Fonts/VT323-Regular");
+        buttonStyle.normal.textColor = Color.black;
+        buttonStyle.hover.textColor = Color.black;
+        buttonStyle.active.textColor = Color.black;
+        buttonStyle.normal.background = MakeTex(2, 2, new Color(0.9607844f, 0.7294118f, 0.9215687f));
+        buttonStyle.hover.background = MakeTex(2, 2, new Color(1f, 0.8820755f, 0.980345f));
+        buttonStyle.active.background = MakeTex(2, 2, new Color(0.9811321f, 0.513706f, 0.9019072f));
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+
+        GUILayout.BeginArea(new Rect(Screen.width * 0.5f - size / 2.0f, Screen.height * 0.5f - size / 2.0f + size + spacing, size, arrowsHeight));
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("<", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(50)))
+        if (GUILayout.Button("<", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(arrowsHeight)))
         {
             int colorID = (ColorID - 1 + gameManager.playerTextures.Length) % gameManager.playerTextures.Length;
             CmdChangeColorID(colorID);
         }
-        if (GUILayout.Button(">", GUILayout.ExpandWidth(true), GUILayout.MaxHeight(50)))
+        if (GUILayout.Button(">", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(arrowsHeight)))
         {
             int colorID = (ColorID + 1) % gameManager.playerTextures.Length;
             CmdChangeColorID(colorID);
         }
         GUILayout.EndHorizontal();
         GUILayout.EndArea();
+    }
+
+    public void ChangeColor(int direction)
+    {
+        GameNetworkManager gameManager = NetworkManager.singleton as GameNetworkManager;
+        if (!gameManager) return;
+        int colorID = (ColorID + direction + gameManager.playerTextures.Length) % gameManager.playerTextures.Length;
+        CmdChangeColorID(colorID);
+    }
+
+    public void MainMenuButton()
+    {
+        GameNetworkManager gameManager = NetworkManager.singleton as GameNetworkManager;
+        gameManager.ReturnToMainMenu();
+    }
+
+    private Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; i++)
+        {
+            pix[i] = col;
+        }
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
     }
 
     [Command]
--- Assets/Scripts/NetworkDiscoveryGUI.cs
+++ Assets/Scripts/NetworkDiscoveryGUI.cs
@@ -1,14 +1,19 @@
 using Mirror;
 using Mirror.Discovery;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkDiscoveryGUI : MonoBehaviour
 {
     public NetworkDiscovery networkDiscovery;
+    public GameObject serversPopup;
+    public Transform serversList;
+    public GameObject buttonPrefab;
     readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
 
-    private bool showServers = false;
+    private bool shouldReloadServers = false;
 
     private void Awake()
     {
@@ -27,80 +32,32 @@ public class NetworkDiscoveryGUI : MonoBehaviour
 
         if (!NetworkClient.isConnected && !NetworkServer.active && !NetworkClient.active)
         {
-            drawTitle();
-            if (showServers)
-                drawServerListGUI();
-            else
-                drawButtonsGUI();
+            if (shouldReloadServers && serversPopup != null)
+            {
+                if (serversPopup.activeSelf)
+                {
+                    PopulateServers();
+                    shouldReloadServers = false;
+                }
+            }
         }
     }
 
-    private void drawTitle()
+    public void PopulateServers()
     {
-        Rect textRect = new Rect(0, 0, Screen.width, Screen.height * 0.5f);
-        GUI.Label(textRect, "Czarodziejki ze Skaryszewa", new GUIStyle(GUI.skin.label)
+        foreach (Transform child in serversList)
         {
-            fontSize = 80,
-            alignment = TextAnchor.MiddleCenter,
-            wordWrap = true
-        });
-    }
-
-    private void drawButtonsGUI()
-    {
-        var buttonStyle = new GUIStyle(GUI.skin.button)
-        {
-            fontSize = 48,
-            alignment = TextAnchor.MiddleCenter
-        };
-        GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.5f));
-        GUILayout.BeginVertical();
-
-        if (GUILayout.Button("Start Host", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
-        {
-            discoveredServers.Clear();
-            NetworkManager.singleton.StartHost();
-            networkDiscovery.AdvertiseServer();
+            Destroy(child.gameObject);
         }
-        GUILayout.Space(50);
-        if (GUILayout.Button("Find Servers", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
-        {
-            discoveredServers.Clear();
-            networkDiscovery.StartDiscovery();
-            showServers = true;
-        }
-        GUILayout.Space(50);
-        if (GUILayout.Button("Exit Game", buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
-        {
-            Debug.Log("Exiting Game...");
-            Application.Quit();
-        }
-        GUILayout.EndVertical();
-        GUILayout.EndArea();
-    }
-
-    private void drawServerListGUI()
-    {
-        var buttonStyle = new GUIStyle(GUI.skin.button)
-        {
-            fontSize = 48,
-            alignment = TextAnchor.MiddleCenter
-        };
-        GUILayout.BeginArea(new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.5f));
-        GUILayout.BeginScrollView(new Vector2(0, 0));
         foreach (ServerResponse info in discoveredServers.Values)
-            if (GUILayout.Button(info.EndPoint.Address.ToString(), buttonStyle, GUILayout.ExpandWidth(true), GUILayout.MaxHeight(100)))
-                Connect(info);
-        GUILayout.EndScrollView();
-        GUILayout.EndArea();
-
-        GUILayout.BeginArea(new Rect(Screen.width - 150f, 10f, 140f, 30f));
-        if (GUILayout.Button("Back"))
         {
-            showServers = false;
+            GameObject serverButton = Instantiate(buttonPrefab, serversList);
+            serverButton.GetComponentInChildren<TMP_Text>().text = info.EndPoint.Address.ToString();
+
+            serverButton.GetComponent<Button>().onClick.AddListener(() => Connect(info));
         }
-        GUILayout.EndArea();
     }
+
     public void HostGame()
     {
         discoveredServers.Clear();
@@ -112,6 +69,19 @@ public class NetworkDiscoveryGUI : MonoBehaviour
     {
         discoveredServers.Clear();
         networkDiscovery.StartDiscovery();
+        PopulateServers();
+        serversPopup.SetActive(true);
+    }
+
+    public void CloseServersPopup()
+    {
+        serversPopup.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
     }
 
     void Connect(ServerResponse info)
@@ -124,5 +94,6 @@ public class NetworkDiscoveryGUI : MonoBehaviour
     {
         // Note that you can check the versioning to decide if you can connect to the server or not using this method
         discoveredServers[info.serverId] = info;
+        shouldReloadServers = true;
     }
 }
