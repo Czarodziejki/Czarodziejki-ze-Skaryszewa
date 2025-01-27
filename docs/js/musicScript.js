c5438c5c73b575ec69974745a7e43d3f1cbc7555
ogoras <skus30@wp.pl>
2025-01-27 16:48:21 +0100

    the site remembers if audio is muted

--- docs/js/musicScript.js
+++ docs/js/musicScript.js
@@ -1,17 +1,20 @@
-let audioMuted = false;
+let audioMuted = document.cookie.includes('audioMuted=true') ?? false;
 let audioStarted = true;
 
 var positionAbsolute;
 
 const audioElement = document.getElementById('main-screen-audio');
 audioElement.volume = 0.5;
+
+if (audioMuted) audioElement.muted = true;
+
 try {
     await audioElement.play();
 } catch (error) {
     audioStarted = false;
 }
 
-if (audioStarted) {
+if (audioStarted || audioMuted) {
     addMuteButton();
 } else {
     // add a listener to start audio after the user clicks on the page
@@ -30,7 +33,7 @@ if (audioStarted) {
 function addMuteButton() {
     const muteButton = document.createElement('button');
     muteButton.classList.add('mute-button');
-    muteButton.innerText = '🔊';
+    muteButton.innerText = audioMuted ? '🔈' : '🔊'; 
     document.body.appendChild(muteButton);
 
     if (positionAbsolute) {
@@ -41,11 +44,17 @@ function addMuteButton() {
         if (audioMuted) {
             audioElement.muted = false;
             muteButton.innerText = '🔊';
+            if (!audioStarted) {
+                audioElement.play();
+                audioStarted = true;
+            }
             audioMuted = false;
+            document.cookie = 'audioMuted=false';
         } else {
             audioElement.muted = true;
             muteButton.innerText = '🔈';
             audioMuted = true;
+            document.cookie = 'audioMuted=true';
         }
     });
 }
