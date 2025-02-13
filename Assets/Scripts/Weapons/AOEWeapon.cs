702a5d21d1cade68c144e3196ee68a79c8628d17
Filip Skrzeczkowski <110346745+feedop@users.noreply.github.com>
2025-02-13 21:29:02 +0100

    Aoe weapon FX (#142)
    * Add aoe weapon trails
    
    * Add AOE weapon particle effects
    
    * Add AOE weapon sound

--- Assets/Scripts/Weapons/AOEWeapon.cs
+++ Assets/Scripts/Weapons/AOEWeapon.cs
@@ -23,4 +23,17 @@ public class AOEWeapon : WeaponWithLimitedAmmo
         explodingComp.range = range;
         explodingComp.distanceMultiplier = distanceMultiplier;
     }
+
+    [Client]
+    override protected void SetColors()
+    {
+        var player = playerTransform.GetComponent<PlayerController>();
+        var particleRenderer = burstParticleSystemController.GetComponent<ParticleSystemRenderer>();
+        particleRenderer.material.SetColor("_Color", player.secondaryColor);
+        particleRenderer.trailMaterial.SetColor("_Color", player.secondaryColor);
+
+        var subemitters = burstParticleSystemController.GetComponent<ParticleSystem>().subEmitters;
+        var particleSubemitter = subemitters.GetSubEmitterSystem(0).GetComponent<ParticleSystemRenderer>();
+        particleSubemitter.material.SetColor("_Color", player.ternaryColor);
+    }
 }
