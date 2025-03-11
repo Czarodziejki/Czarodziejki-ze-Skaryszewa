950aca15b5ed8534c5af54cd9f674286edadc351
Szymon Kuś <57366786+ogoras@users.noreply.github.com>
2025-03-11 10:27:11 +0100

    Esc menu (#152)
    * simple pause menu
    
    * fixed issues with movement while paused
    
    * esc menu bigger buttons, "Leave" moved
    
    * Fix going back to room with ESC menu
    
    ---------
    
    Co-authored-by: Piotr Nieciecki <piotr.nieciecki01@gmail.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using Mirror.Examples.Common;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,7 @@ public class PlayerController : BasePlayerController
     public float speed = 8f;
     public float jumpingPower = 15f;
     public GameObject crosshair;
+    private GameObject myCrosshair = null;
     public Vector2 boxSize;
     public float castDistance;
 
@@ -32,6 +34,17 @@ public class PlayerController : BasePlayerController
     private InputAction jumpAction;
     private Vector2 previousMovementInput;
 
+    private InputAction pauseAction;
+    private bool _paused = false;
+    public bool Paused { 
+        get => _paused;
+        private set
+        {
+            if (myCrosshair != null) myCrosshair.GetComponent<CrosshairController>().SetPaused(value);
+            _paused = value;
+        }
+    }
+
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -53,34 +66,39 @@ public class PlayerController : BasePlayerController
         }
 
         jumpAction = InputSystem.actions.FindAction("Jump");
+        pauseAction = InputSystem.actions.FindAction("Pause");
     }
 
 
-	private void SetupLocalPlayerCrosshair()
+    private void SetupLocalPlayerCrosshair()
     {
         if (isLocalPlayer)
-		    Instantiate(crosshair, gameObject.transform);
-	}
+            myCrosshair = Instantiate(crosshair, gameObject.transform);
+    }
 
 
     void Update()
     {
+        if (pauseAction.triggered) Paused = !Paused;
+
         if (!isLocalPlayer)
             return;
 
-        Vector2 movement = moveAction.ReadValue<Vector2>();
+        Vector2 movement = Paused ? Vector2.zero : moveAction.ReadValue<Vector2>();
 
         horizontal = movement.x;
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
-        bool jump = jumpAction.triggered || (movement.y > 0 && previousMovementInput.y <= 0);
+        if (!Paused) {
+            bool jump = jumpAction.triggered || (movement.y > 0 && previousMovementInput.y <= 0);
 
-        previousMovementInput = movement;
+            previousMovementInput = movement;
 
-        if (jump && IsGrounded())
-        {
-            animator.SetBool("IsJumping", true);
-            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocityX, jumpingPower);
+            if (jump && IsGrounded())
+            {
+                animator.SetBool("IsJumping", true);
+                rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocityX, jumpingPower);
+            }
         }
 
         if (IsGrounded() && rigidBody.linearVelocityY <= 0.01f)
@@ -91,6 +109,54 @@ public class PlayerController : BasePlayerController
         animator.SetFloat("yVelocity", rigidBody.linearVelocityY > 0.0f ? 1.0f : -1.0f);
 
         Flip();
+    }
+
+    public void OnGUI()
+    {
+        if (!isLocalPlayer || !Paused) return;
+
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.font = Resources.Load<Font>("Fonts/VT323-Regular");
+        buttonStyle.normal.textColor = Color.black;
+        buttonStyle.hover.textColor = Color.black;
+        buttonStyle.active.textColor = Color.black;
+        buttonStyle.fontSize = 64;
+        buttonStyle.normal.background = MakeTex(2, 2, new Color(0.9607844f, 0.7294118f, 0.9215687f));
+        buttonStyle.hover.background = MakeTex(2, 2, new Color(1f, 0.8820755f, 0.980345f));
+        buttonStyle.active.background = MakeTex(2, 2, new Color(0.9811321f, 0.513706f, 0.9019072f));
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+        float buttonWidth = 500;
+        float buttonHeight = 100;
+        if (GUI.Button(new Rect((Screen.width - buttonWidth) / 2.0f, (int)(Screen.height / 2 - 1.5 * buttonHeight), buttonWidth, buttonHeight), "RESUME", buttonStyle))
+        {
+            Paused = false;
+        }
+        GameNetworkManager manager = NetworkManager.singleton as GameNetworkManager;
+        bool isHost = manager.IsHost();
+        if (GUI.Button(new Rect((Screen.width - buttonWidth) / 2.0f, (int)(Screen.height / 2 + 0.5 * buttonHeight), buttonWidth, buttonHeight), isHost ? "RETURN TO ROOM" : "LEAVE", buttonStyle))
+        {
+            if (isHost)
+            {
+                manager.ReturnToRoom();
+            }
+            else
+            {
+                manager.StopClient();
+            }
+        }
+    }
+
+    private Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; i++)
+        {
+            pix[i] = col;
+        }
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
     }
 
     private void FixedUpdate()
