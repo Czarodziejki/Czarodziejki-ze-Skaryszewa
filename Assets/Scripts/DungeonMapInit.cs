55dc06e31fcc727b1d8f18e4b8631efc52c4eecd
Filip Skrzeczkowski <110346745+feedop@users.noreply.github.com>
2025-03-11 11:41:41 +0100

    Add proper dungeon music (#162)

--- Assets/Scripts/DungeonMapInit.cs
+++ Assets/Scripts/DungeonMapInit.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 class DungeonMapInit : NetworkBehaviour
 {
+    public AudioClip music;
     private void Start()
     {
         GameObject light = GameObject.Find("Global Light 2D");
         if (light != null)
             light.SetActive(false);
+
+        GameObject musicPlayer = GameObject.Find("Basic music player");
+        if (musicPlayer != null)
+        {
+            var audio = musicPlayer.GetComponent<AudioSource>();
+            audio.resource = music;
+            audio.Play();
+        }        
     }
 
 }
